ae5f56d9a0cb4828bb12de3e294c7e519ac1884f
Anna Williamson <acwilliamson97@gmail.com>
2020-05-11 18:21:49 -0700

    open weather map api call almost working

---
+++ test-sunset-time-api.js
@@ -0,0 +1,63 @@
+var apiKey = "457cbbad81bb1f84bf029b7dd8674602";
+var placeInput = "San Diego";
+var place = "San Diego";
+var buttonsReturned = [];
+var buttons = [];
+var i = 0;
+
+$(".append-city").append("<button class='history'>" + place + "</button> <br>")
+
+function appendCityButton() {
+    if (placeInput != "") {
+        place = placeInput;
+        if (buttons.includes(place)) {
+            return;
+        } else {
+            $(".append-city").append("<button class='history'>" + place + "</button> <br>")
+            buttons.push(place);
+            localStorage.setItem("cities", JSON.stringify(buttons));
+        }
+    } else {
+        return;
+    }
+}
+
+if (localStorage.getItem("cities") !== null) {
+    buttonsReturned = localStorage.getItem("cities");
+    buttons = JSON.parse(buttonsReturned);
+
+    while (i < buttons.length) {
+        $(".append-city").append("<button class='history'>" + buttons[i] + "</button> <br>")
+        i++
+    }
+}
+
+function currentWeather() {
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + place + "&appid=" + apiKey,
+        method: "GET"
+    }).then(function (response) {
+        $("#city-name").text(response.name + " " + moment().month() + "/" + moment().date() + "/" + moment().year());
+        $("#temperature").text("Temperature: " + ((response.main.temp - 273.15) * 9 / 5 + 32).toFixed(1) + "°F");
+        $("#humidity").text("Humidity: " + response.main.humidity + "%");
+        $("#wind-speed").text("Wind Speed: " + response.wind.speed + "MPH");
+        $("#sunset").text("Sunset Time: " + response.sys.sunset);
+        var longitude = response.coord.lon;
+        var latitude = response.coord.lat;
+    });
+}
+
+appendCityButton();
+currentWeather();
+
+$(".query_btn").on("click", function () {
+    placeInput = $(".input").val().trim();
+    appendCityButton();
+    currentWeather();
+});
+
+$(".history").on("click", function () {
+    placeInput = $(this).text();
+    appendCityButton();
+    currentWeather();
+});
