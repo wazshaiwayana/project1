52c48752dacb71f3147519959f363db27586a677
wazhma shaiwayana <wazhma.shaiwayana@gmail.com>
2020-05-12 17:17:59 -0700

    scipt-file

---
+++ script.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+
+     $("#getCity").on("click", function(){
+      
+
+        var city=$("#search").val();
+        city = city.replace(" ", "%");
+     
+
+        var API_KEY = '16502464-7153b39dda231f11debcca3fa';
+        var URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + city + "&image_type=photo";
+   
+        console.log(URL);
+        
+        $.ajax({
+            type: "GET",
+            url: URL,
+            dataType: "json",
+            success: function(data) {
+                
+                 $("#images").empty();
+
+                const imageUrl = data.hits[3].webformatURL;
+                console.log(imageUrl);
+            
+
+                var image = "<img src= '" + imageUrl + "' />";
+                 $("#images").append(image);
+                
+
+        
+            }
+        });
+
+        $.ajax({
+            type: "GET",
+            url: URL,
+            dataType: "json",
+            success: function(data) {
+               
+                const imageUrl = data.hits[5].webformatURL;
+                console.log(imageUrl);
+            
+
+                var image = "<img src= '" + imageUrl + "' />";
+                 $("#images").append(image);
+                
+
+        
+            }
+        });
+
+
+        $.ajax({
+            type: "GET",
+            url: URL,
+            dataType: "json",
+            success: function(data) {
+               
+                const imageUrl = data.hits[8].webformatURL;
+                console.log(imageUrl);
+            
+
+                var image = "<img src= '" + imageUrl + "' />";
+                 $("#images").append(image);
+                
+
+        
+            }
+        });
+
+
+
+        
+
+    })
+})
