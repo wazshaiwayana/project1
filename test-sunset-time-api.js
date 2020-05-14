742a0796c2cab03434658a41de14f205c2b38d37
Anna Williamson <acwilliamson97@gmail.com>
2020-05-13 20:24:05 -0700

    icons

--- test-sunset-time-api.js
+++ test-sunset-time-api.js
@@ -1,17 +1,20 @@
 var apiKey = "457cbbad81bb1f84bf029b7dd8674602";
 var place = "";
-
+ 
 function currentWeather() {
     $.ajax({
         url: "https://api.openweathermap.org/data/2.5/weather?q=" + place + "&appid=" + apiKey,
         method: "GET"
     }).then(function (response) {
+      var iconCode = response.weather[0].icon;
+      var iconurl = "http://openweathermap.org/img/w/" + iconCode + ".png";
         $("#cityName").text(response.name);
         $("#city-name").text( moment().month() + "/" + moment().date() + "/" + moment().year());
         $("#temperature").text("Temperature: " + ((response.main.temp - 273.15) * 9 / 5 + 32).toFixed(1) + "°F");
         $("#humidity").text("Humidity: " + response.main.humidity + "%");
         $("#wind-speed").html("Wind Speed: " + "<br/>" +  response.wind.speed + "MPH");
-       
+        $("#wicon").attr("src", iconurl);
+        
         // this is converting sunset time from unix to regular time, however its in military time. 
         let unix_timestamp = response.sys.sunset
         var date = new Date(unix_timestamp * 1000);
@@ -19,10 +22,10 @@ function currentWeather() {
         var hours= date.getHours();
         var minutes= "0" + date.getMinutes();
         var seconds = "0" + date.getSeconds();
-
+ 
         var formattedTime = hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
         console.log(formattedTime);
-
+ 
         const normalTime = milToStandard(formattedTime);
         $("#sunset").html("Sunset Time: " + "<br/>" + normalTime);
         console.log(normalTime);
@@ -39,17 +42,14 @@ function currentWeather() {
         $("#sunrise").html("Sunrise Time: " + "<br/>" + formattedTime1);       
     });
 }
-
+ 
 currentWeather();
-
+ 
 $(".search-btn").on("click", function () {
     place = $("#search").val().trim();
     currentWeather();
-});
-
-
-
-
+}); 
+ 
 function milToStandard(value) {
   if (value !== null && value !== undefined){ 
     if(value.indexOf('AM') > -1 || value.indexOf('PM') > -1){ 
