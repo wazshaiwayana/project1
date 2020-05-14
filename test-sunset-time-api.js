9722a8f3a497088298b56411b655f9eb28df99f8
Anna Williamson <acwilliamson97@gmail.com>
2020-05-13 19:16:07 -0700

    Added sunrise api call

--- test-sunset-time-api.js
+++ test-sunset-time-api.js
@@ -28,9 +28,17 @@ function currentWeather() {
         $("#sunset").html("Sunset Time: " + "<br/>" + normalTime);
         console.log(normalTime);
         //I think this goes here?????//
-        $("#sunrise").html("Sunrise Time: " + "<br/>" + "");
-               
-       
+        // this is converting sunset time from unix to regular time, however its in military time. 
+        let unix_timestamp1 = response.sys.sunrise
+        var date = new Date(unix_timestamp1 * 1000);
+        console.log(date);
+        var hours= date.getHours();
+        var minutes= "0" + date.getMinutes();
+        var seconds = "0" + date.getSeconds();
+        
+        var formattedTime1 = hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
+        console.log(formattedTime1);
+        $("#sunrise").html("Sunrise Time: " + "<br/>" + formattedTime1);       
     });
 }
 
